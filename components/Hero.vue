eed10309722e3d6479fc19d34c2a6af1d75d9308
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-23 18:37:55 +0700

    chore: responsive hero for mobile devices, sm devices, md devices, lg devices, xl devices and 2xl devices

---
+++ components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="w-11/12 md:w-[740px] lg:w-[1000px] xl:w-[1250px] 2xl:w-[1500px] mx-auto mt-20 sm:mt-16"
+  >
+    <div
+      class="flex flex-col md:flex md:flex-row md:gap-14 lg:gap-10 md:items-center md:justify-between"
+    >
+      <p
+        class="text-[44px] md:text-[100px] font-bold text-[#000000] text-center"
+      >
+        Blog.
+      </p>
+      <p class="text-center md:text-start mt-3 font-normal text-lg">
+        A statically generated blog example using
+        <a
+          href="https://nuxt.com/"
+          target="_blank"
+          class="underline hover:text-[#0070f3] hover:transition"
+        >
+          Nuxt</a
+        >
+        and Markdown.
+      </p>
+    </div>
+  </div>
+</template>
