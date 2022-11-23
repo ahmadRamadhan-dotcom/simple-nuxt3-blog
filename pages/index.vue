20da0e5daa2bfd63a1d369854e6b72143e7938a9
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-23 21:24:27 +0700

    chore: responsive blog post without content

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <Header />
+    <main>
+      <Hero />
+      <div
+        class="mt-16 grid place-content-center place-items-center lg:place-items-start gap-8 lg:gap-16 xl:gap-24"
+      >
+        <div
+          class="w-11/12 md:w-[740px] lg:w-[1000px] xl:w-[1250px] 2xl:w-[1500px] border-8 h-[10em] md:min-h-[30em] md:col-span-2"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit
+          architecto ut sequi harum quod minus, dolore animi vel tenetur
+          incidunt.
+        </div>
+        <div
+          class="w-11/12 md:w-[360px] lg:w-[450px] xl:w-[570px] 2xl:w-[700px] border-8 h-[10em] md:min-h-[20em]"
+        >
+          <img src="/cover.jpg" alt="" />
+        </div>
+        <div
+          class="w-11/12 md:w-[360px] lg:w-[450px] xl:w-[570px] 2xl:w-[700px] border-8 h-[10em] md:min-h-[20em]"
+        ></div>
+      </div>
+    </main>
+  </div>
+</template>
